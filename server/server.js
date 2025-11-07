829fbc8d92714fbdf4759cde10dd805cdd1bdc1c
Muzammal Hussain <mhussain02345@gmail.com>
2025-11-07 23:57:47 +0500

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/../renderer'));
+
+io.on('connection', socket => {
+  console.log('New connection:', socket.id);
+
+  socket.on('offer', offer => socket.broadcast.emit('offer', offer));
+  socket.on('answer', answer => socket.broadcast.emit('answer', answer));
+  socket.on('candidate', candidate => socket.broadcast.emit('candidate', candidate));
+
+  socket.on('disconnect', () => console.log('Disconnected:', socket.id));
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
